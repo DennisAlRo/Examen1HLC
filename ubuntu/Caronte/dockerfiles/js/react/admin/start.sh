b3ccd1f9744e6af8da5aa731267fe2023c4e5184
DennisAlRo <dennisalvarezrodri941@gmail.com>
2024-12-12 09:27:03 +0000

    Commit Final 10:27

--- ubuntu/Caronte/dockerfiles/js/react/admin/start.sh
+++ ubuntu/Caronte/dockerfiles/js/react/admin/start.sh
@@ -7,7 +7,7 @@ config_react():
   else 
     print "EL archivo se encuentra en el lugar";
   
-  npm start from port 3000;
+    do npm start from port 3000;
   
     if "build" == ${PROYECTO}
     do run build 
